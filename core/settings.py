67eb2708f05ee08bbec5deeef6c4e98d551c29c0
VladBell <v.kolokolchikoff@yandex.ru>
2021-10-07 11:27:58 +0300

    feat: added rest_framework and deleted pycache

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'ckeditor_uploader',
+    'rest_framework',
     'articleapp',
     'shopapp',
 ]
--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from core import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 
