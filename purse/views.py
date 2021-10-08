a4cf42a13bceb841e477f7ff6f636782ff0c38f5
VladBell <v.kolokolchikoff@yandex.ru>
2021-10-08 13:40:49 +0300

    feat: wallets users are added in project

--- purse/views.py
+++ purse/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from purse.models import PurseModel
 from purse.serializers import WalletSerializers
 
 
 class UserWalletView(APIView):
+    """Баланс пользователя"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         queryset = get_object_or_404(PurseModel, user_id=request.user)
