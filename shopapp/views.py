fdbd6d1545db8e1825c5e429d680dfbfb3d3a1d8
VladBell <v.kolokolchikoff@yandex.ru>
2021-10-08 13:40:00 +0300

    fix: fixed view in ShopApp

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,9 +1,5 @@
-from django.db.models import Count, F
-from django.db.models.functions import ExtractMonth, ExtractYear
-from rest_framework.decorators import action
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 
 from shopapp.filters import ProductFilter
@@ -12,6 +8,8 @@ from shopapp.models import Product
 
 
 class ProductsViewSet(ReadOnlyModelViewSet):
+    """Просмотр товаров/товара"""
+
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ProductFilter
     queryset = Product.objects.all()
